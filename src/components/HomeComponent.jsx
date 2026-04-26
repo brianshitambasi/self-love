673424f91600a9ea830bc42a0d80eac45f4b7d1b
brayo <brianshitambasi270@gmail.com>
2026-04-26 13:54:17 +0300

    nbbmm

--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -375,16 +375,19 @@ const HomeComponent = () => {
     { icon: "fa-handshake", title: "Business Consulting", desc: "Strategic growth planning" }
   ];
 
-  const links = [
-    { icon: "fa-gem", title: "GO DIAMOND PROJECT", color: "#ffd700", url: "/diamond" },
-    { icon: "fa-github", title: "GITHUB", color: "#ffffff", url: "https://github.com/brianshitambasi" },
-    { icon: "fa-linkedin", title: "LINKEDIN", color: "#0077b5", url: "https://www.linkedin.com/in/brianshitambasi" },
-    { icon: "fa-twitter", title: "X (TWITTER)", color: "#1DA1F2", url: "https://twitter.com/brianshitambasi" },
-    { icon: "fa-facebook", title: "FACEBOOK", color: "#3b5998", url: "https://facebook.com/brianshitambasi" },
-    { icon: "fa-instagram", title: "INSTAGRAM", color: "#e4405f", url: "https://instagram.com/brianshitambasi" },
-    { icon: "fa-youtube", title: "YOUTUBE", color: "#FF0000", url: "https://youtube.com/@brianshitambasi" },
-    { icon: "fa-envelope", title: "EMAIL ME", color: "#ffd700", url: "mailto:brianshtambasi270@gmail.com" }
-  ];
+
+   const links = [
+  { icon: "fa-gem", title: "GO DIAMOND PROJECT", color: "#ffd700", url: "/diamond" },
+  { icon: "fa-github", title: "GITHUB", color: "#ffffff", url: "https://github.com/brianshitambasi" },
+  { icon: "fa-linkedin", title: "LINKEDIN", color: "#0077b5", url: "https://www.linkedin.com/in/brian-shitambasi-613050396" },
+  { icon: "fa-twitter", title: "X (TWITTER)", color: "#1DA1F2", url: "https://twitter.com/brianshitambasi" },
+  { icon: "fa-facebook", title: "FACEBOOK", color: "#3b5998", url: "https://facebook.com/BrianJecks" },
+  { icon: "fa-instagram", title: "INSTAGRAM", color: "#e4405f", url: "https://instagram.com/ceo_brian58" },
+  { icon: "fa-tiktok", title: "TIKTOK", color: "#000000", url: "https://tiktok.com/@.brian005" },
+  { icon: "fa-youtube", title: "YOUTUBE", color: "#FF0000", url: "https://youtube.com/@yobby-5" },
+  { icon: "fa-envelope", title: "EMAIL ME", color: "#ffd700", url: "mailto:brianshtambasi270@gmail.com" }
+];
+
 
   return (
     <>
